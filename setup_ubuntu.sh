da4f8739d9e9044ede5c2dd85d7d8c918b350122
Luigi <luinardi@users.noreply.github.com>
2023-10-02 15:33:23 +0200

    Reordering commands to give more structure on setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,9 +1,4 @@
-#Mount data directory
-sudo mkdir /mnt/data
-sudo mkfs -t ext4 /dev/$VAR || { echo 'Unable to mount disk' ; exit 1; }
-sudo mount -t ext4 /dev/$VAR /mnt/data || { echo 'Unable to mount disk' ; exit 1; }
-
-#Install ubuntu packages
+# Install ubuntu packages
 sudo apt update
 sudo apt -y upgrade
 sudo apt-get -y install python3-pip python-dev openjdk-17-jre-headless
@@ -11,7 +6,7 @@ sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python3.8 /usr/bin/python
 sudo pip3 install psutil
 
-#Install PostgreSQL:
+# Install PostgreSQL:
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 sudo apt update
@@ -28,18 +23,22 @@ cd target && tar xvzf benchbase-postgres.tgz
 mv benchbase-postgres ../..
 cd ../..
 rm -rf benchbase
-
 mv -v synthetic_workload/benchbase_config_files/* benchbase-postgres/config/postgres/
 mv -v synthetic_workload/loader.py benchbase-postgres/
 mv -v synthetic_workload/runner.py benchbase-postgres/
 
-# Copy the data from the old directory to the mounted one.
+# Copy the data from the old directory to the mounted one
 sudo -i -u postgres psql -c "CREATE USER admin WITH LOGIN SUPERUSER PASSWORD 'password';"
 sudo -i -u postgres psql -c "CREATE DATABASE benchbase;"
-sudo rsync -av /var/lib/postgresql /mnt/data
 sudo bash -c 'echo "data_directory = '\''/mnt/data/postgresql/14/main'\''" >> /etc/postgresql/14/main/conf.d/initial.conf'
 sudo bash -c 'echo "max_connections = 450" >> /etc/postgresql/14/main/conf.d/initial.conf'
 sudo bash -c 'echo "max_pred_locks_per_transaction = 500" >> /etc/postgresql/14/main/conf.d/initial.conf'   # Necessary for the Twitter benchmark to run at all
+
+# Mount data directory (the directory will be mounted until you stop your cloud instance, in which case you need to rerun all the commands below)
+sudo mkdir /mnt/data
+sudo mkfs -t ext4 /dev/$VAR || { echo 'Unable to mount disk' ; exit 1; }
+sudo mount -t ext4 /dev/$VAR /mnt/data || { echo 'Unable to mount disk' ; exit 1; }
+sudo rsync -av /var/lib/postgresql /mnt/data
 sudo chmod -R 750 /mnt/data/postgresql
 sudo chown -R postgres:postgres /mnt/data/postgresql
 sudo service postgresql restart
